9069791e875479f7790da65186b9592ef48900eb
Hayab123 <y210177@mail.ryukoku.ac.jp>
2023-12-18 21:14:03 +0900

    no message

--- Assets/korabo/Y210177/素材_ファイル/セーブポイント/TP.cs
+++ Assets/korabo/Y210177/素材_ファイル/セーブポイント/TP.cs
@@ -14,7 +14,7 @@ public class TP : MonoBehaviour
     {
         // (0, 0, 0) (-13, 34, 18) (20, 50 ,24)
         controller = GetComponent<CharacterController>();
-        warpPosition = new Vector3(0, 0, 0);
+        warpPosition = new Vector3(20, 50, 24);
     }
     private void OnTriggerEnter(Collider other)
     {
