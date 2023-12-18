f68f07f417e738bc77dc0548484441143a6a439b
Kaito-111 <106918233+Kaito-111@users.noreply.github.com>
2023-12-18 11:34:54 +0900

    修正しました

--- Assets/korabo/y210189/script/ladder.cs
+++ Assets/korabo/y210189/script/ladder.cs
@@ -1,54 +1,7 @@
-/*using UnityEngine;
+using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class ladder : MonoBehaviour
 {
-    public float climbSpeed = 2f; // 梯子を登る速さ
-    private CharacterController characterController;
-    private bool isOnLadder = false;
-    //private GameObject Player;
-
-    void Start()
-    {
-        GameObject Player = GameObject.Find("unitychan_dynamic");
-        characterController = Player.GetComponent<CharacterController>();
-    }
-
-    void Update()
-    {
-        // 梯子に触れているかどうかの判定
-        if (isOnLadder)
-        {
-            //Debug.Log("a");
-            // 上下の入力を取得
-            if(Input.GetKey(KeyCode.L)){
-                // 移動ベクトルの作成（上方向に移動）
-                Vector3 moveDirection = new Vector3(0, climbSpeed, 0);
-                moveDirection = transform.TransformDirection(moveDirection);
-                Debug.Log("a");
-                // 梯子の上昇
-                characterController.Move(moveDirection * climbSpeed * Time.deltaTime);
-            }
-        }
-    }
-
-    // 梯子に触れたときに呼ばれるメソッド
-    void OnTriggerEnter(Collider other)
-    {
-        //Debug.Log("b");
-        if (other.CompareTag("Player"))
-        {
-            isOnLadder = true;
-        }
-    }
-
-    // 梯子から離れたときに呼ばれるメソッド
-    void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            isOnLadder = false;
-        }
-    }
+    [TextArea(3, 10)] // 行数3から10までの複数行のテキストエリア
+    public string multilineText = "はしごを2個以上連続で置く場合は、カプセルコライダーを全体で一つにしてください。カプセルコライダーの大きさは、使ってみて自分で修正してください。　カプセルコライダーのtriggerは外さないでください。";
 }
-
-*/
